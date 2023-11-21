ff086578dca7f988c436bed9e35f7e2a78314d05
sungohki <sokim1201@naver.com>
2023-11-21 22:37:45 +0900

    modified: add enemy death effect

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject particlePrefab;
     void Start()
     {
         EnemyManager.instance.AddEnemy(this);
     }
 
     private void OnDestroy() {
+        Instantiate(particlePrefab, transform.position, transform.rotation);
         EnemyManager.instance.RemoveEnemy(this);
     }
 }
